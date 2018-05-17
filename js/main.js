4726d48fd3c03d67394a46fc45842d4be3eac3e4
Tina <tparkmedia@gmail.com>
2018-05-17 15:45:16 -0700

    creat new item prepended to the list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-// your code here:
+var $main = $('.main')
+var $list = $('.list')
+var $form = $('.form')
+var $formContainer = $('.form-container')
+var $button = $('.btn')
+var $toDo = $('#todo')
+
+// prepend new item to list
+$form.on('submit', function(evt) {
+    evt.preventDefault()
+    var toDoValue = $toDo.val()
+    
+    $list.prepend(`
+        <p>
+            <input type="checkbox">
+            <i class="glyphicon glyphicon-star"></i>
+            <span>${toDoValue}</span>
+            <i class="glyphicon glyphicon-remove"></i>
+        </p>
+    `)
+    $toDo.val("")
+})
+
+
+
+
+
+
